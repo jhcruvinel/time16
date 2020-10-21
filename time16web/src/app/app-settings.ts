789d2a78fcfebda161ddae37b1eb2a4cac70b1d1
Lucas Vinicius Bibiano Thome <lucasvbt@trt3.jus.br>
2020-10-21 00:31:12 -0300

    fix: Correção do endpoint

--- time16web/src/app/app-settings.ts
+++ time16web/src/app/app-settings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
-  public static API_ENDPOINT: string = 'http://localhost:5000/api';
+  public static API_ENDPOINT: string = 'http://time16-sanjus.ddns.net:5000/api';
   public static CONF_SNACK: any = { duration: 5000 };
 }
